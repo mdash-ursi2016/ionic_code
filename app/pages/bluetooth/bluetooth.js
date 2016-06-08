b668f16687ff8b9537ee171b01abd7ae5d241b5d
Nathaniel <natgreen@vassar.edu>
2016-06-08 10:18:36 -0400

    major updates...everything is changed

--- app/pages/bluetooth/bluetooth.js
+++ app/pages/bluetooth/bluetooth.js
@@ -1,9 +1,129 @@
 import {Page} from 'ionic-angular';
+import {BLE} from 'ionic-native';
+
 
 @Page({
   templateUrl: 'build/pages/bluetooth/bluetooth.html'
 })
-export class BluetoothPage {
-  constructor() { }
 
+export class BluetoothPage {
+  constructor() {}
+  //constructor() {}
+
+  scan() {
+  
+      var foundDevice;
+      
+      var scanInfo = { service: '180d',
+		       heartrate: '2a37',
+		       timeout: 25 };
+      
+      /* If a Bluetooth device has been found, this will be true.
+	 Prevents incorrect status. */
+
+      /* This is the object that controls connection to a device.
+	 All of its members are functions that will be called sequentially,
+	 starting with init() */
+      var bleApp = {
+
+
+	  init: function() {
+	      statusDiv.innerHTML = "Initializing";
+	      BluetoothPage.connect();
+	      document.addEventListener('deviceready', this.startScan, false);
+	      //this.startScan();   //<-- Use when debugging on server instead of previous line
+	      console.log("Operations completed");
+	  },
+
+	  startScan: function() {
+	      foundDevice = false;
+	      statusDiv.innerHTML = "Scanning";
+	      
+	      /* Not sure how to implement a failure function with subscriptions, so this isn't
+		 currently used */
+	      function scanFailure(reason) {
+		  alert("Scan Failed");
+		  statusDiv.innerHTML = "Scan error";
+		  console.log("Could not find a BLE device");
+	      }
+
+	      /* Notify the user of the device and unsubscribe. Then connect to device. */
+	      function scanSuccess(peripheral) {
+		  console.log("Found a BLE device: " + JSON.stringify(peripheral));
+		  foundDevice = true;
+		  subscription.unsubscribe();
+		  statusDiv.innerHTML = "Found device";
+		  alert("Device Found: " + peripheral.name);
+		  var connectSub = BLE.connect(peripheral.id).subscribe(result => {
+									  //connectSub.unsubscribe();
+		                                                          connect();
+									  bleApp.connected(peripheral);
+									});
+	      }
+
+	      /* Subscribe to the scan using heart rate service. If a device is found, pass it and the subscription along */
+	      var subscription = BLE.scan([scanInfo.service], scanInfo.timeout).subscribe(device => 
+											  {scanSuccess(device,subscription);});
+	      
+	      /* If no device is found, that sucks. Update */
+	      setTimeout(function() {
+		  if (!foundDevice) {
+		      statusDiv.innerHTML = "No device found";
+		  }
+	      }, (scanInfo.timeout * 1000));
+	  },
+
+	  /* When the device has successfully connected */
+	  connected: function(peripheral) {
+	      statusDiv.innerHTML = "Connected to " + peripheral.name;
+	      connectDiv.innerHTML = peripheral.id;
+	      
+	      /* Start reading data and update the HTML to reflect this */
+	      BLE.startNotification(peripheral.id, scanInfo.service, scanInfo.heartrate).subscribe(buffer =>
+										{var data = new Uint8Array(buffer);
+										 content.innerHTML = data[1];
+										});
+	  }
+
+      }
+
+     
+     bleApp.init();
+  }   
+
+  static connect() {alert("WE HAVE CONNECTED, COMMANDER");}
+  
+  disconnect() {
+      var devID = connectDiv.innerHTML;
+      if (devID != null) {
+	  BLE.disconnect(devID).then(
+	      function(result) {
+		  statusDiv.innerHTML = "Disconnected";
+		  connectDiv.innerHTML = "";
+		  content.innerHTML = "Data will display here";
+		  alert("Disconnected Successfully");
+	      },
+	      function(result) {
+		  statusDiv.innerHTML = "Error Disconnecting";
+	      });
+      }
+      else
+	  { alert("You aren't connected to a device");}
+  }
+
+  /* Observe if Bluetooth is enabled or not */
+  bleCheck() {
+     /* isEnabled() returns a Promise */
+     BLE.isEnabled().then(function(result) {alert("Bluetooth is enabled");}, 
+			  function(result) {alert("Bluetooth is *not* enabled");}
+			 );
+    
+  }
+  /* Enable Bluetooth in the case that it isn't alreay enabled */
+  enable() {
+     /* enable() returns a Promise */
+     BLE.enable().then(function(result) {alert("Bluetooth has been enabled");},
+		       function(result) {alert("Bluetooth has *not* been enabled");}
+		      );
+  }
 }
--- app/pages/settings/settings.js
+++ app/pages/settings/settings.js
@@ -1,4 +1,5 @@
 import {Page, NavController, NavParams} from 'ionic-angular';
+import {Geolocation} from 'ionic-native';
 import {AboutPage} from '/home/natgreen/URSI/ursiApp/app/pages/about/about';
 import {BluetoothPage} from '/home/natgreen/URSI/ursiApp/app/pages/bluetooth/bluetooth';
 import {ServerPage} from '/home/natgreen/URSI/ursiApp/app/pages/server/server';
@@ -12,14 +13,13 @@ export class SettingsPage {
     return [[NavController], [NavParams]];
   }
 
-  constructor(nav, navParams) {
+  constructor(nav, navParams, platform) {
     this.nav = nav;
-
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
 
     /* The icons that will be used in the list labels */
-    this.icons = ['bluetooth','wifi'];
+    this.icons = ['bluetooth','desktop', 'locate'];
 
     this.items = [];
 
@@ -36,8 +36,16 @@ export class SettingsPage {
       icon: this.icons[1],
       id: 1
     });
-    
+
+    this.items.push({
+      title: 'Current Coordinates',
+      icon: this.icons[2],
+      id: 2
+    });
+
   }
+
+	
 
   /* If a list item is touched, check its ID and go to the correct corresponding page */
   itemTapped(event, item) {
@@ -49,6 +57,13 @@ export class SettingsPage {
     else if (item["id"] == 1)
       { console.log("Server page");
         this.nav.push(ServerPage);
+      }
+
+    /* Or in this case, give the user their coordinates because why not */
+    else if (item["id"] == 2)
+      {
+	Geolocation.getCurrentPosition().then((resp) => {
+	    alert("Latitude: " + resp.coords.latitude + "\nLongitude: " + resp.coords.longitude); });
       }
 
     /* If the ID is not recognized, burn the world to the ground */
