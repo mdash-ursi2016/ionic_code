c12c8cabb806e2fcbb41fd1ddc090b5fed18f2dd
Nathaniel <natgreen@vassar.edu>
2016-06-13 17:58:48 -0400

    chart.js imports added

--- app/pages/home/home.js
+++ app/pages/home/home.js
@@ -1,5 +1,6 @@
 import {BluetoothPage} from '../bluetooth/bluetooth';
 import {Page, Storage, SqlStorage} from 'ionic-angular';
+import {Chart} from 'chart.js';
 
 @Page({
   templateUrl: 'build/pages/home/home.html'
@@ -10,10 +11,10 @@ export class HomePage {
       BluetoothPage.storage = new Storage(SqlStorage);
   }
 
-  onPageLoaded()
+  onPageLoaded()with the
     {
 	/* Currently breaks if you select the Homp Page *from* the Home Page */
-	HomePage.makeChart();
+	HomePage.makeChart2();
     }
 
     /* Clear all data in storage and update the chart.
@@ -78,5 +79,45 @@ export class HomePage {
       HomePage.chart.draw(HomePage.data, HomePage.options);
   }
 
+  
+    static makeChart2() {
+	var ctx = document.getElementById("chart");
+	var myChart = new Chart(ctx, {
+	    type: 'bar',
+	    data: {
+		labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+		datasets: [{
+		    label: '# of Votes',
+		    data: [12, 19, 3, 5, 2, 3],
+		    backgroundColor: [
+			'rgba(255, 99, 132, 0.2)',
+			'rgba(54, 162, 235, 0.2)',
+			'rgba(255, 206, 86, 0.2)',
+			'rgba(75, 192, 192, 0.2)',
+			'rgba(153, 102, 255, 0.2)',
+			'rgba(255, 159, 64, 0.2)'
+		    ],
+		    borderColor: [
+			'rgba(255,99,132,1)',
+			'rgba(54, 162, 235, 1)',
+			'rgba(255, 206, 86, 1)',
+			'rgba(75, 192, 192, 1)',
+			'rgba(153, 102, 255, 1)',
+			'rgba(255, 159, 64, 1)'
+		    ],
+		    borderWidth: 1
+		}]
+	    },
+	    options: {
+		scales: {
+		    yAxes: [{
+			ticks: {
+			    beginAtZero:true
+			}
+		    }]
+		}
+	    }
+	});
+    }
 
 }
